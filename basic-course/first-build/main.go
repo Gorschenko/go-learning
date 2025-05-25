9a66b5df9e0ff1380fc673a891fdbd6ad7f20f5c
Alexey G. <alexey.gor@safit.pro>
2025-05-25 18:02:11 +0300

    add: add string logic

--- basic-course/first-build/main.go
+++ basic-course/first-build/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// .Println после вывода перейдет на новую строку,
+	// тогда не нужно указывать \n
 	fmt.Print("Калькулятор индекса массы тела \n")
 	// Альтернативный вариант записи переменной
 	// Позволяет указать тип после переменной
@@ -19,10 +21,12 @@ func main() {
 	// Можно сразу объявить две переменных
 	// var userHeight, userKg float64 = 1.8, 100
 	// userHeight, userKg := 1.8, 100
+
 	fmt.Print("Введите свой рост в метрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес в кг: ")
 	fmt.Scan((&userKg))
+
 	// Переназначить const не получится
 	// У переменной untyped int тип, так как
 	// не явно был задан тип. Это упрощает работу с float64
@@ -30,7 +34,8 @@ func main() {
 
 	// Второй вариант работы с типом:
 	// var IMT = float64(userKg) / userHeight
-	fmt.Print("Ваш индекс массы тела: ")
 	var IMT = userKg / math.Pow(userHeight, IMTPower)
+	fmt.Printf("Ваш индекс массы тела %v", IMT)
+	fmt.Print("Ваш индекс массы тела: ")
 	fmt.Print(IMT)
 }
