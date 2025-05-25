9732b36af1008ba84b236e69f3e50ec3d176bde1
Alexey G. <alexey.gor@safit.pro>
2025-05-25 17:06:58 +0300

    mod: simple changes

--- basic-course/first-build/main.go
+++ basic-course/first-build/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Привет")
+	fmt.Print("Калькулятор индекса массы тела \n")
+	// Альтернативный вариант записи переменной
+	// Позволяет указать тип после переменной
+	// var userHeight: float64 = 1.83
+	// Позволяет позже присвоить значение
+	// var userHeight float64
+	// userHeight = 1.83
+	userHeight := 1.83
+	var userKg float64 = 83
+	userHeight = 2.0
+	// Можно сразу объявить две переменных
+	// var userHeight, userKg float64 = 1.8, 100
+	// userHeight, userKg := 1.8, 100
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в кг: ")
+	fmt.Scan((&userKg))
+	// Переназначить const не получится
+	// У переменной untyped int тип, так как
+	// не явно был задан тип. Это упрощает работу с float64
+	const IMTPower = 2
+
+	// Второй вариант работы с типом:
+	// var IMT = float64(userKg) / userHeight
+	fmt.Print("Ваш индекс массы тела: ")
+	var IMT = userKg / math.Pow(userHeight, IMTPower)
+	fmt.Print(IMT)
 }
