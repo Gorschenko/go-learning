9550b9610a3527bcb5fc7e9115fb32deb89fe660
Горщенко Алексей <alexey.gor@safit.pro>
2026-01-02 23:59:35 +0300

    mod: cars module

--- microservices/apps/auth/internal/cars/handler.go
+++ microservices/apps/auth/internal/cars/handler.go
@@ -2,22 +2,20 @@ package cars
 
 import (
 	"net/http"
-	cars_api "pkg/api/cars"
-	"pkg/middlewares"
 )
 
 func NewCarsHandler(router *http.ServeMux, dependencies *CarsHandlerDependencies) {
-	handler := &CarsHandler{
-		CarsService: dependencies.CarsService,
-	}
+	// handler := &CarsHandler{
+	// 	CarsService: dependencies.CarsService,
+	// }
 
-	addCarURL := cars_api.AddCarMethod + " " + cars_api.AddCarPath
-	router.Handle(
-		addCarURL,
-		middlewares.ValidateBody[cars_api.AddCarRequestBodyDto](handler.AddCar()),
-	)
+	// addCarURL := cars_api.AddCarMethod + " " + cars_api.AddCarPath
+	// router.Handle(
+	// 	addCarURL,
+	// 	middlewares.ValidateBody[cars_api.AddCarRequestBodyDto](handler.AddCar()),
+	// )
 }
 
-func (h *CarsHandler) AddCar() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
-}
+// func (h *CarsHandler) AddCar() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {}
+// }
