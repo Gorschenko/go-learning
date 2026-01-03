08ae82705b19c3278f6eab2d794b5e9d78162843
Горщенко Алексей <alexey.gor@safit.pro>
2026-01-04 00:20:34 +0300

    add: users test

--- microservices/apps/auth/internal/users/e2e/get_one_test.go
+++ microservices/apps/auth/internal/users/e2e/get_one_test.go
@@ -1,9 +1,11 @@
 package users_e2e
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
+	auth_api "pkg/api/auth"
 	users_api "pkg/api/users"
 	"strconv"
 	"testing"
@@ -15,12 +17,37 @@ import (
 
 func TestGetOneUser(t *testing.T) {
 	t.Run("Positive", func(t *testing.T) {
+		t.Run(strconv.Itoa(http.StatusOK), func(t *testing.T) {
+			URL := testServer.URL + auth_api.RegisterPath
+			requestRegisterBody := auth_api.RegisterRequestBodyDto{
+				Email:    gofakeit.Email(),
+				Password: gofakeit.Password(false, false, true, false, false, 5),
+				Name:     gofakeit.Name(),
+			}
+			requestRegisterBodyString, _ := json.Marshal(&requestRegisterBody)
+			responseRegister, _ := http.Post(URL, "application/json", bytes.NewReader(requestRegisterBodyString))
 
+			assert.NotNil(t, responseRegister)
+
+			requestGetOneUserQuery := users_api.GetOneRequestQueryDto{
+				Email: requestRegisterBody.Email,
+			}
+			requestGetOneQueryValues, _ := query.Values(requestGetOneUserQuery)
+			URL = testServer.URL + users_api.GetOnePath
+			URL = URL + "?" + requestGetOneQueryValues.Encode()
+
+			responseGetOne, _ := http.Get(URL)
+			responseGetOneBodyString, _ := io.ReadAll(responseGetOne.Body)
+			var responseGetOneBody users_api.GetOneResponseBodyDto
+			json.Unmarshal(responseGetOneBodyString, &responseGetOneBody)
+
+			assert.Equal(t, http.StatusOK, responseGetOne.StatusCode)
+			assert.Equal(t, requestRegisterBody.Email, responseGetOneBody.User.Email)
+		})
 	})
 
 	t.Run("Negative", func(t *testing.T) {
 		t.Run(strconv.Itoa(http.StatusNotFound), func(t *testing.T) {
-
 			requestQuery := users_api.GetOneRequestQueryDto{
 				ID: gofakeit.Int(),
 			}
