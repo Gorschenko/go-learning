cd86a41c11349849105a21ab882f31f360eae1c8
Горщенко Алексей <alexey.gor@safit.pro>
2026-01-06 02:55:47 +0300

    add: users tst

--- microservices/apps/auth/internal/e2e/update_one_test.go
+++ microservices/apps/auth/internal/e2e/update_one_test.go
@@ -43,4 +43,33 @@ func TestUpdateOneUser(t *testing.T) {
 			assert.Equal(t, 1, responseBody.Count)
 		})
 	})
+
+	t.Run("Negative", func(t *testing.T) {
+		t.Run(strconv.Itoa(http.StatusNotFound), func(t *testing.T) {
+			filters := users_api.UserFiltersDto{
+				Email: gofakeit.Name(),
+			}
+
+			update := users_api.UserUpdateDto{
+				Name: gofakeit.Name(),
+			}
+
+			requestBody := users_api.UpdateOneRequestBodyDto{
+				Filters: filters,
+				Update:  update,
+			}
+
+			URL := testServer.URL + users_api.UpdateOnePath
+			var responseBody users_api.UpdateOneResponseBodyDto
+
+			response, _ := resty.
+				New().
+				R().
+				SetBody(requestBody).
+				SetResult(&responseBody).
+				Execute(users_api.UpdateOneMethod, URL)
+
+			assert.Equal(t, http.StatusNotFound, response.StatusCode())
+		})
+	})
 }
